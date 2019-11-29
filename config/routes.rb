53f43da6f82e954435d83098f41633f41297e697
Ben Fanning fanninggg@gmail.com <fanninggg@gmail.com>
2019-11-29 16:52:53 +0000

    Added i18n setup, navbar, footer, faqs

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,14 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    get 'jobs/new'
-    get 'jobs/create'
-    get 'jobs/edit'
-    get 'jobs/update'
-    get 'jobs/destroy'
+  scope "/:locale" do
+    root to: 'pages#home'
+    get '/faq', to: 'pages#faq'
+    devise_for :users
+    namespace :admin do
+      resources :countries, only: [:index, :new, :create, :edit, :update, :destroy]
+      resources :posts, only: [:new, :create, :edit, :update, :destroy]
+      resources :jobs, only: [:new, :create, :edit, :update, :destroy]
+    end
+    resources :posts, only: [:index, :show]
+    resources :jobs, only: [:index, :show]
   end
-  get 'jobs/index'
-  get 'jobs/show'
-  root to: 'pages#home'
-  devise_for :users
-  namespace :admin do
-    resources :countries, only: [:index, :new, :create, :edit, :update, :destroy]
-    resources :posts, only: [:new, :create, :edit, :update, :destroy]
-    resources :jobs, only: [:new, :create, :edit, :update, :destroy]
-  end
-  resources :posts, only: [:index, :show]
-  resources :jobs, only: [:index, :show]
 end
