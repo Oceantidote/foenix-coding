c8055fca2dc0eb07e174d4f31c14cdf839a5436b
Ife Odugbesan <ifeodugbesan@yahoo.com>
2019-12-12 18:01:25 +0000

    started product page

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
     get '/inks_and_consumables', to: 'pages#inks_and_consumables'
     get '/distributor', to: 'pages#distributor'
     get '/cost_of_ownership', to: 'pages#cost_of_ownership'
+    get '/fxone_remote', to: 'pages#fxone_remote'
     get '/send_email', to: 'pages#send_email'
   # end
 end
