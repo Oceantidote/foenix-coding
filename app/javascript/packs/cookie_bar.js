8868a53204cce4e174b9501eb2f2c9b7117e6ec9
Ife Odugbesan <ifeodugbesan@yahoo.com>
2019-12-18 13:55:55 +0000

    added cookie animation

--- app/javascript/packs/cookie_bar.js
+++ app/javascript/packs/cookie_bar.js
@@ -1,6 +1,13 @@
 cookieClose = document.querySelector('.close-cookie-bar');
 cookieBar = document.querySelector('.cookie-bar');
 
+if (cookieBar) {
+  setTimeout(function() {
+      cookieBar.style.bottom = '0' + 'px'
+    }, 2000
+  )
+}
+
 if (cookieClose) {
   cookieClose.addEventListener('click', () => {
     cookieBar.style.bottom = '-200px'
