ee3908288bd14cc01dc0f35b457c4a253f2678be
Ife Odugbesan <ifeodugbesan@yahoo.com>
2020-01-07 15:08:42 +0000

    More items checked off

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,15 @@
 class Post < ApplicationRecord
   mount_uploader :image, PhotoUploader
+  after_save :update_featured
 
   def reading_time
     (text.split(' ').size / 150.to_f).ceil
+  end
+
+  def update_featured
+    if self.featured == true
+      @posts = Post.where(featured: true).where.not(id: self.id)
+      @posts.update_all(featured: false)
+    end
   end
 end
