6aaadb379664bb8389c1f059c445d00844b3a3e0
leonard percival <leonardpercival@gmail.com>
2020-01-15 17:20:39 +0000

    double frame allow

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,7 +35,7 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
   config.action_dispatch.default_headers = {
-    'X-Frame-Options' => 'allow-from http://www.foenixcoding.com/'
+    'X-Frame-Options' => 'allow-from http://www.foenixcoding.com/, http://www.sauven-marking.com/'
   }
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
